2cf6858f6ce454b1b56d7a4e471cfa34e4a13159
JuanPDev11 <lucumi763@gmail.com>
2024-03-14 20:30:27 -0500

    Update data 1.3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetCategoriesComponent } from './components/get-categories/get-categories.component';
+import { CreateUpdateComponent } from './components/create-update/create-update.component';
+import { CreateUpdateProductComponent } from './components/create-update-product/create-update-product.component';
+import { GetProductComponent } from './components/get-product/get-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'categoryIndex', pathMatch: 'full' },
+  { path: 'categoryIndex', component: GetCategoriesComponent },
+  { path: 'createUpdate', component: CreateUpdateComponent },
+  { path: 'createUpdate/:id', component: CreateUpdateComponent },
+  { path: 'productIndex', component: GetProductComponent },
+  { path: 'creUpd', component: CreateUpdateProductComponent },
+  { path: 'creUpd/:id', component: CreateUpdateProductComponent },
+  { path:'**', redirectTo: 'categoryIndex' , pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,31 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetCategoriesComponent } from './components/get-categories/get-categories.component';
+import { CreateUpdateComponent } from './components/create-update/create-update.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { SharedModule } from './shared/shared.module';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { GetProductComponent } from './components/get-product/get-product.component';
+import { CreateUpdateProductComponent } from './components/create-update-product/create-update-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetCategoriesComponent,
+    CreateUpdateComponent,
+    GetProductComponent,
+    CreateUpdateProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
