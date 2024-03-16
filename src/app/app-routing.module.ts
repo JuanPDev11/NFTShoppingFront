ec2f0d4db55c463979223ae8b689830ff2d997ed
JuanPDev11 <lucumi763@gmail.com>
2024-03-16 18:30:17 -0500

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { GetCategoriesComponent } from './components/get-categories/get-categori
 import { CreateUpdateComponent } from './components/create-update/create-update.component';
 import { CreateUpdateProductComponent } from './components/create-update-product/create-update-product.component';
 import { GetProductComponent } from './components/get-product/get-product.component';
+import { ArtistCreupComponent } from './components/artist-creup/artist-creup.component';
+import { GetArtistComponent } from './components/get-artist/GetArtistComponent';
 
 const routes: Routes = [
   { path: '', redirectTo: 'categoryIndex', pathMatch: 'full' },
@@ -13,6 +15,10 @@ const routes: Routes = [
   { path: 'productIndex', component: GetProductComponent },
   { path: 'creUpd', component: CreateUpdateProductComponent },
   { path: 'creUpd/:id', component: CreateUpdateProductComponent },
+  { path: 'artistCreUpd', component: ArtistCreupComponent},
+  { path: 'artistCreUpd/:id', component: ArtistCreupComponent },
+  { path: 'artistIndex', component: GetArtistComponent},
+
   { path:'**', redirectTo: 'categoryIndex' , pathMatch:'full'}
 ];
 
