5f645372386909f5f4d3c313b6fd4344f4e35a43
JuanPDev11 <lucumi763@gmail.com>
2024-03-29 19:36:50 -0500

    update home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
+import { info } from 'console';
+import { take } from 'rxjs';
+import { response } from 'express';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private isLocalStorageAvailable = typeof localStorage !== 'undefined';
+  info: any;
+  categories: any;
+  constructor(private _serviceI: CategoryService) { }
+
+  ngOnInit(): void {
+    if (this.isLocalStorageAvailable) {
+      this.getInfo();
+      this.getCategories();
+    }
+  }
+
+  getInfo() {
+    this._serviceI.getProduct().subscribe({
+      next: (info:any[]) => {
+        this.info = info.slice(0,12);
+      }
+    })
+  }
+
+  getCategories() {
+    this._serviceI.homeCategories().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.categories = response;
+      }
+    })
+  }
+
+
 
 }
