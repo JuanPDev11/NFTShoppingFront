d3d7e7f7301dc898ce2af9b4d4b7989a2dc42b9c
JuanPDev11 <lucumi763@gmail.com>
2024-04-03 21:38:48 -0500

    update details componenet ts

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Scroll } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -8,28 +8,42 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent implements OnInit{
+  private isWindowAvailable = typeof window !== 'undefined';
   ID!: number;
   info: any;
   product: any;
   products: any;
-
-  constructor(private _service: CategoryService, private _route: ActivatedRoute) {
-    this.ID = Number(_route.snapshot.paramMap.get("id"));
+  
+  constructor(private _redirect: Router, private _service: CategoryService, private _route: ActivatedRoute) {
+    
   }
 
 
   ngOnInit(): void {
+    this.ID = Number(this._route.snapshot.paramMap.get("id"));
     this.getData();
+    if (this.isWindowAvailable) {
+      window.scrollTo(0, 0);
+    }
   }
 
   getData() {
+
     this._service.getInfo(this.ID).subscribe({
       next: (data) => {
         console.log(data);
         this.product = data.productD;
         this.products = data.products;
       }
-    })
+    });   
   }
 
+  redirect(newId: number) {
+    this._redirect.navigateByUrl(`/details/${newId}`, { skipLocationChange: true }).then(() => {
+      this._redirect.navigate([this._redirect.url]);
+      this.ngOnInit();
+
+    })
+
+  }
 }
