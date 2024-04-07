249e3987bdac03d6d90cd33a89d2a1cf6f1c8118
JuanPDev11 <lucumi763@gmail.com>
2024-04-07 18:41:13 -0500

    update summary component

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-summary',
@@ -7,8 +8,18 @@ import { CategoryService } from '../../services/category.service';
   styleUrl: './summary.component.scss'
 })
 export class SummaryComponent implements OnInit {
-
-  constructor(private _serviceC:CategoryService) {}
+  carts: any;
+  formOrder!: FormGroup;
+  constructor(private _serviceC: CategoryService, private _fb: FormBuilder) {
+    this.formOrder = _fb.group({
+      name: [''],
+      phone: [''],
+      address: [''],
+      city: [''],
+      state: [''],
+      postalCode:['']
+    })
+  }
 
   ngOnInit(): void {
     this.getOrder();
@@ -18,7 +29,32 @@ export class SummaryComponent implements OnInit {
     this._serviceC.getSummary().subscribe({
       next: (data: any) => {
         console.log(data);
+        this.carts = data;
+        this.formOrder.patchValue({
+          name: data.orderHeader.name,
+          phone: data.orderHeader.phone,
+          address: data.orderHeader.address,
+          city: data.orderHeader.city,
+          state: data.orderHeader.state,
+        })
       }
     })
   }
+
+  submit() {
+    const formData = new FormData();
+    formData.append("name", this.formOrder.get("name")?.value);
+    formData.append("phone", this.formOrder.get("phone")?.value);
+    formData.append("address", this.formOrder.get("address")?.value);
+    formData.append("city", this.formOrder.get("city")?.value);
+    formData.append("state", this.formOrder.get("state")?.value);
+    formData.append("postalCode", this.formOrder.get("postalCode")?.value);
+
+    this._serviceC.postSummary(formData).subscribe({
+      next: (data:any) => {
+        window.location.href = data.url;
+      }
+    })
+  }
+
 }
