ae70b30eec3c9e841235108e41c69fbe71f25593
JuanPDev11 <lucumi763@gmail.com>
2024-04-09 18:27:14 -0500

    update navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../../services/account.service';
+import { Category } from '../../interface/category';
+import { CategoryService } from '../../services/category.service';
+import { Router } from '@angular/router';
+import { CartComponent } from '../cart/cart.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
-
-  constructor(public service:AccountService) { }
+export class NavbarComponent implements OnInit {
+  private isLocalStorageAvailable = typeof localStorage !== 'undefined';
+  
+  
+  carts: any;
+  constructor(private _redirect: Router, public service: AccountService, public app:AppComponent) {
+    
+  }
+    
 
   logout() {
     this.service.logout();
+    //this._redirect.navigateByUrl('');
+    //location.reload();
   }
+
+  ngOnInit(): void {
+    
+    
+  }
+
+  
 }
