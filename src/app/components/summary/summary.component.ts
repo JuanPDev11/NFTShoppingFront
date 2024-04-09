e2ee1dd6a4074143d60cd6ae9dcfe20a7ff4b66e
JuanPDev11 <lucumi763@gmail.com>
2024-04-09 18:26:29 -0500

    update summary component

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-summary',
@@ -12,12 +13,12 @@ export class SummaryComponent implements OnInit {
   formOrder!: FormGroup;
   constructor(private _serviceC: CategoryService, private _fb: FormBuilder) {
     this.formOrder = _fb.group({
-      name: [''],
-      phone: [''],
-      address: [''],
-      city: [''],
-      state: [''],
-      postalCode:['']
+      name: ['',[Validators.required]],
+      phone: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      postalCode: ['', [Validators.required]]
     })
   }
 
@@ -42,19 +43,28 @@ export class SummaryComponent implements OnInit {
   }
 
   submit() {
-    const formData = new FormData();
-    formData.append("name", this.formOrder.get("name")?.value);
-    formData.append("phone", this.formOrder.get("phone")?.value);
-    formData.append("address", this.formOrder.get("address")?.value);
-    formData.append("city", this.formOrder.get("city")?.value);
-    formData.append("state", this.formOrder.get("state")?.value);
-    formData.append("postalCode", this.formOrder.get("postalCode")?.value);
+    if (this.formOrder.valid) {
+      const formData = new FormData();
+      formData.append("name", this.formOrder.get("name")?.value);
+      formData.append("phone", this.formOrder.get("phone")?.value);
+      formData.append("address", this.formOrder.get("address")?.value);
+      formData.append("city", this.formOrder.get("city")?.value);
+      formData.append("state", this.formOrder.get("state")?.value);
+      formData.append("postalCode", this.formOrder.get("postalCode")?.value);
 
-    this._serviceC.postSummary(formData).subscribe({
-      next: (data:any) => {
-        window.location.href = data.url;
-      }
-    })
+      this._serviceC.postSummary(formData).subscribe({
+        next: (data: any) => {
+          window.location.href = data.url;
+        }
+      })
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops..',
+        text: 'Please complete fields form requireds'
+      });
+    }
+    
   }
 
 }
