f8171f5daf3f074a6407cce67634908bff185a86
JuanPDev11 <lucumi763@gmail.com>
2024-04-10 19:19:07 -0500

    update authorization guard

--- src/app/guards/authorization.guard.ts
+++ src/app/guards/authorization.guard.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 import { AccountModule } from '../account/account.module';
 import { AccountService } from '../services/account.service';
 import { SharedService } from '../shared/shared.service';
 import { User } from '../interface/user';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationGuard  {
-
+  private isLocalStorage = typeof localStorage !== undefined;
   constructor(private serviceAccount: AccountService, private serviceShared: SharedService,
   private _redirect:Router) { }
 
@@ -18,17 +19,16 @@ export class AuthorizationGuard  {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> {
     
-    return this.serviceAccount.user$.pipe(
-      map((user: User | null) => {
-        if (user) {
-          return true;
-        } else {
-          this.serviceShared.showModal("restricted Area", "leave inmediately!");
-          this._redirect.navigate(['/account/login'], { queryParams: {returnUrl: state.url } });
-          return false;
-        }
-      })
-    )
+    let local = localStorage.getItem(environment.userKey);
+    if (local !== null) {
+      console.log("from true");
+      return of(true);
+    } else {
+      console.log("from false");
+      this.serviceShared.showModal("Please Login","You must be logged")
+      this._redirect.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
+      return of(false);
+    }
   }
 }
 
