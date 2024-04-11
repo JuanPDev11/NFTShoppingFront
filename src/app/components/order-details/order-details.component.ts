2350c3fd1a51ef9eff4619e038eff9c079444580
JuanPDev11 <lucumi763@gmail.com>
2024-04-10 19:19:28 -0500

    update orderDetails Component

--- src/app/components/order-details/order-details.component.ts
+++ src/app/components/order-details/order-details.component.ts
@@ -24,7 +24,8 @@ export class OrderDetailsComponent implements OnInit {
   constructor(private _service:CategoryService,private _aroute: ActivatedRoute,private _fb:FormBuilder) {
     this.ID = Number(this._aroute.snapshot.paramMap.get("id"));
     this.formDetails = _fb.group({
-      name: [{ value: '' ,disabled:this.showForm}],
+      id:[this.ID],
+      name: [{ value: '' ,disabled:true}],
       phone: [{ value: '', disabled: true }],
       address: [{ value: '', disabled: true }],
       city: [{ value: '', disabled: true }],
@@ -59,7 +60,9 @@ export class OrderDetailsComponent implements OnInit {
           dateOrder: data.orderHeader.dateOfOrder,
           orderStatus: data.orderHeader.orderStatus,
           paymentStatus: data.orderHeader.paymentStatus,
-          datePayment: data.orderHeader.dateOfPayment,         
+          datePayment: data.orderHeader.dateOfPayment,
+          carrier: data.orderHeader.carrier,
+          trackingNumber: data.orderHeader.trackingNumber
         })
         this.productDetails = data.orderDetails;
         this.orderDetails = data.orderHeader;
@@ -68,6 +71,32 @@ export class OrderDetailsComponent implements OnInit {
   }
 
   submit() {
+    if (this.formDetails.valid) {
+      let formData = new FormData();
+      formData.append("id", this.formDetails.get("id")?.value);
+      formData.append("name", this.formDetails.get("name")?.value);
+      formData.append("phone", this.formDetails.get("phone")?.value);
+      formData.append("address", this.formDetails.get("address")?.value);
+      formData.append("city", this.formDetails.get("city")?.value);
+      formData.append("state", this.formDetails.get("state")?.value);
+      formData.append("email", this.formDetails.get("email")?.value);
+      formData.append("dateOrder", this.formDetails.get("dateOrder")?.value);
+      formData.append("orderStatus", this.formDetails.get("orderStatus")?.value);
+      formData.append("paymentStatus", this.formDetails.get("paymentStatus")?.value);
+      formData.append("datePayment", this.formDetails.get("datePayment")?.value);
+      formData.append("carrier", this.formDetails.get("carrier")?.value);
+      formData.append("trackingNumber", this.formDetails.get("trackingNumber")?.value);
+
+      this._service.updateOrder(formData).subscribe({
+        next: (data: any) => {
+          console.log(data);
+          
+        }
+      })
+
+      
+
+    }
 
   }
 
