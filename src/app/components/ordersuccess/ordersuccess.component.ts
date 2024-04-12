c8c24273f77b6386be789b38804fed8276311abf
JuanPDev11 <lucumi763@gmail.com>
2024-04-12 18:27:26 -0500

    update ordersuccess ts, scss and html

--- src/app/components/ordersuccess/ordersuccess.component.ts
+++ src/app/components/ordersuccess/ordersuccess.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
 import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-ordersuccess',
@@ -8,10 +10,10 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './ordersuccess.component.scss'
 })
 export class OrdersuccessComponent implements OnInit{
-  private isLocal = typeof localStorage !== undefined;
+  private isLocal = this.app.isLocalStorageAvailable;
   ID!: number;
   orderId: any;
-  constructor(private _serviceC:CategoryService,private _route:ActivatedRoute)
+  constructor(private _serviceC:CategoryService,private _route:ActivatedRoute, public app:AppComponent)
   {
     this.ID = Number(this._route.snapshot.paramMap.get("id"));
   }
@@ -26,6 +28,24 @@ export class OrdersuccessComponent implements OnInit{
     this._serviceC.paymentRes(this.ID).subscribe({
       next: id => {
         this.orderId = id;
+      }
+    })
+  }
+
+  details() {
+    this._serviceC.getDetails(this.orderId).subscribe({
+      next: (data: any) => {
+        Swal.fire({
+          icon: 'info',
+          title: 'Order Details',
+          html: `<div style="text-align: left;">
+                     <strong>Customer: </strong> ${data.orderHeader.applicationUser.name}<br>
+                     <strong>Address: </strong> ${data.orderHeader.address}-${data.orderHeader.city}-${data.orderHeader.state}<br>
+                     <strong>Order Status: </strong> ${data.orderHeader.orderStatus}<br>
+                     <strong>Payment Status: </strong> ${data.orderHeader.paymentStatus}<br>
+                     <strong>Your Order will be shipped within 3 to 5 working days</strong>
+                 </div>`
+        })
       }
     })
   }
