47a4a26a0330c45d5ca13af079d3dffead9f93c9
JuanPDev11 <lucumi763@gmail.com>
2024-04-12 18:24:29 -0500

    add creUpdUsr Component

---
+++ src/app/components/cre-upd-usr/cre-upd-usr.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AccountModule } from '../../account/account.module';
+import { AccountService } from '../../services/account.service';
+
+@Component({
+  selector: 'app-cre-upd-usr',
+  templateUrl: './cre-upd-usr.component.html',
+  styleUrl: './cre-upd-usr.component.scss'
+})
+export class CreUpdUsrComponent implements OnInit{
+  formRegister!: FormGroup;
+
+  constructor(private _serviceA: AccountService, private _fb: FormBuilder) {
+    this.formRegister = _fb.group({
+      Name:[''],
+      Phone: [''],
+      Email: [''],
+      Password: [''],
+      Role: [''],
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  submit() {
+    this._serviceA.register(this.formRegister.value).subscribe({
+      next: (data: any)=>{
+        console.log(data);
+      }
+    })
+  }
+
+}
