9931d73705519f4bd93a24104f516f97621fd81b
JuanPDev11 <lucumi763@gmail.com>
2024-04-12 18:25:52 -0500

    update roleGuard

--- src/app/guards/role-guard.guard.ts
+++ src/app/guards/role-guard.guard.ts
@@ -9,24 +9,38 @@ import { SharedService } from '../shared/shared.service';
   providedIn: 'root'
 })
 export class roleGuardGuard implements CanActivate {
-
+  islocal = this.checkLocal();
   constructor(private _serviceR:RoleServiceService, private _redirect:Router, private _ServiceS:SharedService) { }
   
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> {
 
-    const expectedRole = route.data['expectedRole'];
-    const actualRole = this._serviceR.getRole();
-    if (actualRole !== expectedRole) {
-      this._ServiceS.showModal('Unauthorized', 'this area is for admin');
-      this._redirect.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
-      return of(false);
-    }
+    const expectedRoleAdmin = route.data['expectedRoleAdmin'];
+    const expectedRoleEmployee = route.data['expectedRoleEmployee'];
+    if (this.islocal) {
 
+      const actualRole = this._serviceR.getRole();
+      if (actualRole !== expectedRoleAdmin && actualRole !== expectedRoleEmployee) {
+        this._ServiceS.showModal('Unauthorized', 'this area is for admin');
+        this._redirect.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
+        return of(false);
+      }
+
+      return of(true);
+    }
     return of(true);
+
   }
 
-
+  checkLocal() {
+    try {
+      localStorage.setItem('test', 'test');
+      localStorage.removeItem('test');
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 
 
 }
