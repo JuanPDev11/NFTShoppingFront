d670aaaa0ca24f98f9c665fc2135d9099e144534
JuanPDev11 <lucumi763@gmail.com>
2024-04-14 19:28:38 -0500

    update cart ts

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -10,26 +10,72 @@ import { AccountService } from '../../services/account.service';
 export class CartComponent implements OnInit{
   private isLocalStorageAvailable = typeof localStorage !== 'undefined'; 
   carts!: any[];
-
-  constructor(private _serviceC: CategoryService, private _serviceA:AccountService) { }
+  orders!: any;
+  constructor(public _serviceC: CategoryService, private _serviceA:AccountService) { }
 
   ngOnInit(): void {
     if (this.isLocalStorageAvailable) {
-      
+      /*this._serviceC.getCart();*/
       this.getCarts();
 
     }
   }
 
-
-  
-
-
   getCarts() {
-    this._serviceC.getCart().subscribe({
+    this._serviceC.getCart();
+    this._serviceC.cartObservable.subscribe({
       next: (data: any) => {
         console.log(data);
         this.carts = data;
+        
+      }
+    })
+
+    this._serviceC.getTotal().subscribe({
+      next: (data: any) => {
+        this.orders = data;
+      }
+    })
+    
+      
+    
+  }
+
+  increment(count:number,productId:number) {
+    this._serviceC.increment(count, productId).subscribe({
+      next: data => {
+        console.log(data);
+        this.ngOnInit();
+
+      }
+    })
+  }
+
+  decrement(count:number,productId:number) {
+    this._serviceC.decrement(count, productId).subscribe({
+      next: data => {
+        console.log(data);
+        this.ngOnInit();
+
+      }
+    })
+  }
+
+  delete(id:number) {
+    this._serviceC.deleteCart(id).subscribe({
+      next: data => {
+        console.log(data);
+        this.ngOnInit();
+      }
+    })
+  }
+
+  deleteRange() {
+    this._serviceC.deleteRange().subscribe({
+      next: data => {
+        console.log(data);
+        this.ngOnInit();
+
       }
     })
   }
