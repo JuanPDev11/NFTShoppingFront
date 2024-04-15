47c7c7b2d95c82d8bca402830e0a7555cc55afe3
JuanPDev11 <lucumi763@gmail.com>
2024-04-14 19:26:16 -0500

    update details ts

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
-import { ActivatedRoute, Navigation, NavigationStart, Router, Scroll ,Event as EventRouter} from '@angular/router';
+import { ActivatedRoute, Navigation, NavigationStart, Router, Scroll ,Event as EventRouter, RouterStateSnapshot} from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AppComponent } from '../../app.component';
 import { Location } from '@angular/common';
 import Swal from 'sweetalert2';
+import { AccountService } from '../../services/account.service';
+import { User } from '../../interface/user';
+import { map } from 'rxjs';
+import { SharedService } from '../../shared/shared.service';
+import { AuthorizationGuard } from '../../guards/authorization.guard';
 
 @Component({
   selector: 'app-details',
@@ -19,12 +24,12 @@ export class DetailsComponent implements OnInit{
   info: any;
   product: any;
   products: any;
-
+  registered!: boolean;
   formCart!: FormGroup;
   
   constructor(private _redirect: Router, private _service: CategoryService,
-    private _route: ActivatedRoute, private _fb: FormBuilder, public app: AppComponent,
-    private location:Location) {
+    private _account:AccountService,private _route: ActivatedRoute, private _fb: FormBuilder, public app: AppComponent,
+    private location:Location,private _guard:AuthorizationGuard, private _shared:SharedService) {
 
     
       this.formCart = this._fb.group({
@@ -58,21 +63,39 @@ export class DetailsComponent implements OnInit{
   }
 
   submit() {
-    if (this.formCart.valid) {
-      const formData = new FormData();
-      formData.append('productId', this.formCart.get('productId')?.value);
-      formData.append('count', this.formCart.get('count')?.value);
-      this._service.addCart(formData);
+    
+    this._account.user$.subscribe({
+      next: (data: User|null) => {
+        if (data) {
+          this.registered = true;
+        } else {
+          this.registered = false;
+        }
+      }
+    })
+    if (this.registered) {
+      if (this.formCart.valid) {
+        const formData = new FormData();
+        formData.append('productId', this.formCart.get('productId')?.value);
+        formData.append('count', this.formCart.get('count')?.value);
+        this._service.addCart(formData);
 
-      this.location.back();
+        this.location.back();
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Please insert a quantity valid'
+        })
+      }
     } else {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text:'Please insert a quantity valid'
-      })
+      console.log("from else");
+
+      this._shared.showModal("Please LogIn", "You must be logged ");
+      this._redirect.navigate(['/account/login'], { queryParams: { returnUrl: `/details/${this.ID}` } });
+      
     }
-   
+    
     
     
   }
