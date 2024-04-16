e340aa449a92cfba09c1c291046ef25105447a91
JuanPDev11 <lucumi763@gmail.com>
2024-04-16 18:36:27 -0500

    update account routing module

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ComfirmEmailComponent } from './comfirm-email/comfirm-email.component';
+import { CreUpdUsrComponent } from '../components/cre-upd-usr/cre-upd-usr.component';
 
 const routes: Routes = [
   {path:'login' , component:LoginComponent},
   { path: 'register', component: RegisterComponent },
-  { path: 'confirmEmail' , component : ComfirmEmailComponent}
+  { path: 'confirmEmail', component: ComfirmEmailComponent },
+  { path: 'creUpdUsr', component: CreUpdUsrComponent},
+  { path: 'creUpdUsr/:id', component: CreUpdUsrComponent },
 ]
 
 @NgModule({
